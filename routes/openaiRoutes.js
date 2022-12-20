d34fd0ffd882f62c45dce200c1270a8adb3b3072
Ryan Mello <74434148+ryanmello@users.noreply.github.com>
2022-12-19 16:53:43 -0800

    bug fixed

--- routes/openaiRoutes.js
+++ routes/openaiRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const router = express.Routher();
+const router = express.Router();
 
 router.post('/generateimage', (req, res) => {
     res.status(200).json({
         success: true,
-    })
+    });
 });
 
 module.exports = router;
