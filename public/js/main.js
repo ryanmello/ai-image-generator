5c545a40e6a8b311a2ad990c2dec15e8e756c4e7
Ryan Mello <74434148+ryanmello@users.noreply.github.com>
2022-12-22 16:15:53 -0800

    image request function

---
+++ public/js/main.js
@@ -0,0 +1,54 @@
+const { application } = require("express");
+
+function onSubmit(e){
+    e.preventDefault();
+
+    const prompt = document.querySelector('#prompt').value;
+    const size = document.querySelector('#size').value;
+
+    if (prompt === '') {
+        alert('Please add text');
+        return;
+    }
+
+    generateImageRequest(prompt, size);
+}
+
+async function generateImageRequest(prompt, size){
+    try {
+        showSpinner();
+
+        const response = await fetch('/openai/generateimage', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                prompt,
+                size,
+            }),
+        });
+
+        if(!reponse.ok){
+            removeSpinner();
+            throw new Error('image could not be generated');
+        }
+
+        const data = await response.json();
+        console.log(data);
+
+        removeSpinner();
+    } catch(error) {
+        docuemnt.querySelector('.msg').textContent = error;
+    }
+}
+
+function showSpinner(){
+    document.querySelector('.spinner').classList.add('show');
+}
+
+function removeSpinner(){
+    document.querySelector('.spinner').classList.remove('show');
+}
+
+document.querySelector('#image-form').addEventListener('submit', onSubmit);
